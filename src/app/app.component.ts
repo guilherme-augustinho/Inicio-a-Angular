7c595f95453bd8b1a5ff32e68c41d35032c9fa0b
Guilherme Augusto Morais de Proença <guilhermemorais06072005@gmail.com>
2023-11-13 16:56:28 -0300

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,40 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    imports: [
+      CommonModule, RouterOutlet
+    ],
 })
 export class AppComponent {
-  title = 'example-app';
+  mostrarComprados = true;
+  produto = "";
+  list = [
+    { produto: 'arroz', quantidade: 2, comprado: false, delete: "delete" },
+    { produto: 'leite', quantidade: 8, comprado: false, delete: "delete" },
+    { produto: 'nescau', quantidade: 1, comprado : false, delete: "delete" }
+  ]
+
+  markCheckbox(event: any, produto: string){
+    this.list.forEach(it => {
+      if(it.produto == produto)
+        it.comprado = event.target.checked;
+    });
+  }
+
+  showComprados(event: any)
+  {
+    this.mostrarComprados = !event.target.checked;
+  }
+
+  deleteProduct(produto: string)
+  {
+    
+  }
+
 }
